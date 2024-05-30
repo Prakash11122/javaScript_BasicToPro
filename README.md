f0f76cd890368a69c63047ad493bf6dd26d5dbab
prakash kumar pradhan <111282629+Prakash11122@users.noreply.github.com>
2024-05-30 16:29:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# javaScript_BasicToPro
+ javaScript_BasicToPro
---
+++ javaScript_01/test.js
@@ -0,0 +1,18 @@
+const accountId =88889999;
+
+let accountEmail = "prakash@gmail.com"
+
+var accountPassword = "147258i"
+
+accountCity = "Bhunaneswar";
+
+accountEmail = "abcd@gmail.com"
+accountPassword ="prakash12"
+accountCity = "kenderapara"
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+console.log(accountId);
+console.log(accountEmail);
+
